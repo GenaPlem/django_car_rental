092694b8e8537322e5399912e7d0e046b5754247
GenaPlem <shainkill1337@gmail.com>
2023-11-01 12:00:50 +0000

    Fix Allowed hosts

--- car_rental_service/settings.py
+++ car_rental_service/settings.py
@@ -34,7 +34,7 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 if DEBUG:
     ALLOWED_HOSTS = ['127.0.0.1']
 else:
-    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME', '')]
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 # Application definition
 
