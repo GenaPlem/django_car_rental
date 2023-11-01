428bb571f185c6b034a42d02e6b295d450b47e8f
GenaPlem <shainkill1337@gmail.com>
2023-11-01 10:54:55 +0000

    Add custom models for Car and Booking

---
+++ car_rental/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Car(models.Model): 
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.IntegerField()
+    seats = models.IntegerField()
+    color = models.CharField(max_length=20)
+    price_per_day = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f'{self.make} {self.model} ({self.year})'
+    
+    
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    
+    def __str__(self):
+        return f"Booking for {self.car} by {self.user}"
+    
