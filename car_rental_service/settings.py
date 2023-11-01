78bfd981edce357fc8b8baabd2aa60f400ff8c90
GenaPlem <shainkill1337@gmail.com>
2023-11-01 12:04:51 +0000

    Fix debug variable

--- car_rental_service/settings.py
+++ car_rental_service/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 if DEBUG:
     ALLOWED_HOSTS = ['127.0.0.1']
