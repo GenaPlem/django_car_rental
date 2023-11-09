4914ccbe56c662e5ebc99be5c6ac0ee319328f26
GenaPlem <shainkill1337@gmail.com>
2023-11-09 15:04:33 +0000

    Add home page to views

--- car_rental/views.py
+++ car_rental/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Car, Booking
 
-# Create your views here.
+
+# class CarList(generic.ListView):
+#     model = Car
+#     queryset = Car.objects.all()
+#     template_name = ''
+#     paginate_by = 9
+
+
+def home(request):
+    return render(request, 'index.html')
