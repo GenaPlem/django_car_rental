3b4b196a028fde763d4d3354dbb1841b8da20006
GenaPlem <shainkill1337@gmail.com>
2023-11-13 11:26:10 +0000

    Add path to urls.py for CarsList

--- car_rental_service/urls.py
+++ car_rental_service/urls.py
@@ -21,4 +21,5 @@ from car_rental import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('cars/', views.CarsListView.as_view(), name='cars-list'),
 ]
