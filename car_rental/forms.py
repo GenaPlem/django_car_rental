54c16934450fb1ccec8818adbb4551170889ad95
GenaPlem <shainkill1337@gmail.com>
2023-11-20 13:07:04 +0000

    Add form for booking

---
+++ car_rental/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Booking
+
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ('name', 'surname', 'start_date', 'end_date', 'child_seat', 'insurance_type', 'rules_agreement')
+        widgets = {
+            'start_date': forms.DateInput(attrs={'type': 'date'}),
+            'end_date': forms.DateInput(attrs={'type': 'date'}),
+            'rules_agreement': forms.CheckboxInput(attrs={'required': 'required'}),
+        }
+    
+    def clean_rules_agreement(self):
+        rules_agreement = self.cleaned_data.get('rules_agreement')
+        if not rules_agreement:
+            raise forms.ValidationError("You must agree to the rules to proceed.")
+        return rules_agreement
+        
