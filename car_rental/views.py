fcae48413187a9ae729a4e9dac16cba5720be57e
GenaPlem <shainkill1337@gmail.com>
2023-11-20 13:07:24 +0000

    Update Car Details view with form

--- car_rental/views.py
+++ car_rental/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, TemplateView, DetailView
+from django.views.generic.edit import FormMixin
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin # to only authenticated users can fill the form  
 from .models import Car, Booking
+from .forms import BookingForm
 
 
 class HomeView(TemplateView):
@@ -14,6 +18,31 @@ class CarsListView(ListView):
     paginate_by = 6
 
 
-class CarDetailsView(DetailView):
+class CarDetailsView(DetailView, FormMixin):
     model = Car
     template_name = 'car_details.html'
+    form_class = BookingForm
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if 'form' not in context:
+            context['form'] = self.get_form()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        booking = form.save(commit=False)
+        booking.user = self.request.user
+        booking.car = self.object
+        booking.save()
+        return super(CarDetailsView, self).form_valid(form)
