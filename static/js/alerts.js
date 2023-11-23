8ad7936bc64bc58a8f1c4031c7be356ba4f65ab8
GenaPlem <shainkill1337@gmail.com>
2023-11-23 17:53:50 +0000

    Add custom alerts

---
+++ static/js/alerts.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const alert = document.getElementById('alertBox');
+    setTimeout(() => {
+        alert.classList.add('hide')
+        setTimeout(() => {
+            alert.remove()
+        }, 600)
+        }, 5000);
+});
