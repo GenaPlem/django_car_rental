1212bca03d06b9878c3522b637173e068ac0d2d6
GenaPlem <shainkill1337@gmail.com>
2023-11-23 11:25:49 +0000

    Add validations for Datepicker

--- static/js/booking.js
+++ static/js/booking.js
@@ -2,9 +2,9 @@ const startDateInput = document.getElementById('id_start_date');
 const endDateInput = document.getElementById('id_end_date');
 const insuranceTypeSelect = document.getElementById('id_insurance_type');
 const childSeatCheckbox = document.getElementById('id_child_seat');
-const carSeats = document.getElementById('car_seats');
 const pricePerDayElement = document.getElementById('price_per_day');
 const totalPriceElement = document.getElementById('total_price');
+
 /**
  * Function to update booking total price
  */
@@ -12,7 +12,7 @@ const updateTotalPrice = () => {
     let startDate = startDateInput.value;
     let endDate = endDateInput.value;
     let insuranceType = insuranceTypeSelect.value;
-    let childSeat = childSeatCheckbox.checked;
+    let childSeat = childSeatCheckbox ? childSeatCheckbox.checked : false;
     let pricePerDay = pricePerDayElement.textContent.match(/\d+/)[0];
 
     let insuranceCost = insuranceType === 'young' ? 50 : (insuranceType === 'senior' ? 60 : 40);
@@ -56,20 +56,45 @@ const updateEndDateMin = (e) => {
     }
 }
 
-/* Function, If car's seats less than 4 than user can't add a child seat */
-const isChildSeatAvailable = () => {
-    if (Number(carSeats.textContent.split(' ')[1]) < 4) {
-        childSeatCheckbox.setAttribute('disabled', 'true')
+/**
+ * Function to update max booking date for 1 month
+ */
+const updateEndDateMax = (e) => {
+    const selectedDate = e.detail.date;
+    const maxBookingDate = new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, selectedDate.getDate());
+    endDatepicker.setOptions({
+        minDate: selectedDate,
+        maxDate: maxBookingDate
+    });
+
+    if (endDateInput.value && new Date(endDateInput.value) < selectedDate) {
+        endDatepicker.setDate(selectedDate);
     }
+
+    if (endDateInput.value && new Date(endDateInput.value) > maxBookingDate) {
+        endDatepicker.setDate(maxBookingDate)
+    }
+}
+/* If startDateInput already has a value than max date will be after one month */
+if (startDateInput.value) {
+    const selectedDate = new Date(startDateInput.value);
+    const maxBookingDate = new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, selectedDate.getDate());
+
+    endDatepicker.setOptions({
+        minDate: selectedDate,
+        maxDate: maxBookingDate
+    });
 }
 
 /* Booking form listeners */
 startDateInput.addEventListener('changeDate', updateTotalPrice);
 startDateInput.addEventListener('changeDate', updateEndDateMin);
+startDateInput.addEventListener('changeDate', updateEndDateMax);
 endDateInput.addEventListener('changeDate', updateTotalPrice);
 insuranceTypeSelect.addEventListener('change', updateTotalPrice);
-childSeatCheckbox.addEventListener('change', updateTotalPrice);
+if (childSeatCheckbox) {
+    childSeatCheckbox.addEventListener('change', updateTotalPrice);
+}
 
 
-isChildSeatAvailable()
 updateTotalPrice()
