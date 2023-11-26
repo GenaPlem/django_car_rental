02c5341e661398dfcca8e64e74c8315c0dcea5cd
GenaPlem <shainkill1337@gmail.com>
2023-11-26 14:53:22 +0000

    Add tests for Booking Delete View

--- tests/tests_views.py
+++ tests/tests_views.py
@@ -93,7 +93,7 @@ class CarDetailsViewTest(TestCase):
 class ProfileViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        test_user_profile = User.objects.create_user(username='testuser', password='aSdw12465')
+        test_user_profile = User.objects.create_user(username='testuser1', password='aSdw12465')
         test_user_profile.save()
 
         car_image_url = 'https://res.cloudinary.com/dd2fwm3fh/image/upload/v1699883191/xgwfa17rshjqvkaxk2hb.webp'
@@ -125,10 +125,10 @@ class ProfileViewTest(TestCase):
         self.assertIn('/accounts/login/', response.url)
 
     def test_logged_in_uses_correct_template(self):
-        login = self.client.login(username='testuser', password='aSdw12465')
+        login = self.client.login(username='testuser1', password='aSdw12465')
         response = self.client.get(reverse('profile'))
 
-        self.assertEqual(str(response.context['user']), 'testuser')
+        self.assertEqual(str(response.context['user']), 'testuser1')
         self.assertEqual(response.status_code, 200)
 
         self.assertTemplateUsed(response, 'profile.html')
@@ -146,7 +146,7 @@ class ProfileViewTest(TestCase):
                                rules_agreement=True,
                                )
 
-        self.client.login(username='testuser', password='aSdw12465')
+        self.client.login(username='testuser1', password='aSdw12465')
         response = self.client.get(reverse('profile'))
 
         self.assertTrue('bookings' in response.context)
@@ -159,6 +159,7 @@ class BookingEditViewTest(TestCase):
     def setUpTestData(cls):
         test_user = User.objects.create_user(username='testuser', password='wdaSdw12465')
         test_user.save()
+
         car_image_url = 'https://res.cloudinary.com/dd2fwm3fh/image/upload/v1699883191/xgwfa17rshjqvkaxk2hb.webp'
         test_car = Car.objects.create(make='Audi',
                                       model='A4',
@@ -208,6 +209,69 @@ class BookingEditViewTest(TestCase):
         self.assertTrue('booked_dates' in response.context)
         self.assertTrue('car' in response.context)
         self.assertEqual(response.context['car'], booking.car)
+
+
+class BookingDeleteViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        test_user = User.objects.create_user(username='testuser', password='1X<123a+tuK')
+
+        car_image_url = 'https://res.cloudinary.com/dd2fwm3fh/image/upload/v1699883191/xgwfa17rshjqvkaxk2hb.webp'
+
+        test_car = Car.objects.create(make='Audi',
+                                      model='A4',
+                                      year=2020,
+                                      seats=4,
+                                      transmission_type='manual',
+                                      fuel_type='petrol',
+                                      price_per_day=Decimal('200.00'),
+                                      car_image=car_image_url,
+                                      available=True
+                                      )
+
+        Booking.objects.create(user=test_user,
+                               car=test_car,
+                               start_date=date(2024, 1, 1),
+                               end_date=date(2024, 1, 3),
+                               name='TestName',
+                               surname='TestSurname',
+                               child_seat=False,
+                               insurance_type='standard',
+                               rules_agreement=True,
+                               total_price=Decimal('400.00')
+                               )
+
+    def test_view_url_exists_at_desired_location(self):
+        self.client.login(username='testuser', password='1X<123a+tuK')
+        booking = Booking.objects.get(name='TestName')
+        response = self.client.get(f'/booking/{booking.pk}/delete/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        self.client.login(username='testuser', password='1X<123a+tuK')
+        booking = Booking.objects.get(name='TestName')
+        response = self.client.get(reverse('booking_delete', kwargs={'pk': booking.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        self.client.login(username='testuser', password='1X<123a+tuK')
+        booking = Booking.objects.get(name='TestName')
+        response = self.client.get(reverse('booking_delete', kwargs={'pk': booking.pk}))
+        self.assertTemplateUsed(response, 'booking_confirm_delete.html')
+
+    def test_redirect_after_delete(self):
+        self.client.login(username='testuser', password='1X<123a+tuK')
+        booking = Booking.objects.get(name='TestName')
+        response = self.client.post(reverse('booking_delete', kwargs={'pk': booking.pk}))
+        self.assertRedirects(response, reverse('profile'))
+
+    def test_success_message(self):
+        self.client.login(username='testuser', password='1X<123a+tuK')
+        booking = Booking.objects.get(name='TestName')
+        response = self.client.post(reverse('booking_delete', kwargs={'pk': booking.pk}), follow=True)
+        messages = list(response.context['messages'])
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), 'Booking was successfully canceled!')
 
 
 class PrivacyPolicyViewTest(TestCase):
